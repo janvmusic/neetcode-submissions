cbe31a3dfec2aee29c5b3d41a26cb29f557a3efa
Jorge A Nunez <1697540+janvmusic@users.noreply.github.com>
2026-05-23 15:04:07 -0600

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let sIdx = 0;
+        let eIdx = s.length - 1;
+        const isAlphanumeric = c => /[a-zA-Z0-9]/.test(c);
+
+        while(sIdx < eIdx) {
+            let left = s[sIdx];
+            let right = s[eIdx];
+
+            console.log(left, right)
+
+            if (!isAlphanumeric(left)) {
+                sIdx++;
+                continue;
+            }
+
+            if (!isAlphanumeric(right)) {
+                eIdx--;
+                continue;
+            }
+
+            if (left.toLowerCase() !== right.toLowerCase()) {
+                return false;
+            }
+
+            sIdx++;
+            eIdx--;
+        }
+
+        return true;
+    }
+}
